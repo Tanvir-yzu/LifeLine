f71f0c84c1fb2e1d4d34d052eeec82781586f4ed
Tanvir Islam <2020tanvir1971@gmail.com>
2025-09-01 03:27:31 +0800

    desd

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
@@ -6,6 +6,32 @@ import uuid
 # Signal to create UserProfile automatically when User is created
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+class UserManager(BaseUserManager):
+    """Custom user manager for email-based authentication"""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a regular user with an email and password."""
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser with an email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     """Custom User model with email verification and blood donation features"""
@@ -59,6 +85,9 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    # Use custom manager
+    objects = UserManager()
     
     # Fix for reverse accessor clashes
     groups = models.ManyToManyField(
